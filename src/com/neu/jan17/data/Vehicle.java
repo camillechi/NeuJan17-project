ccc36722371f1dc680685ab1f344d9782ee856e1
Siva Dosapati <siva.dosapati@gmail.com>
2017-04-07 22:47:27 -0700

    Project changes..

---
+++ src/com/neu/jan17/data/Vehicle.java
@@ -0,0 +1,12 @@
+package com.neu.jan17.data;
+
+public class Vehicle {
+	private String id;
+	private Integer year;
+	private String make;
+	private String model;
+	private String trim;
+	private Category category;
+	private String bodyType;
+	private Float price;
+}
