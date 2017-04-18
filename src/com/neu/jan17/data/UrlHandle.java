b3d23dfcc7d3eae9633a654a44a9e9b270a7b871
Shiyao Xiao <xiaoshiyao1128@gmail.com>
2017-04-18 10:03:12 -0700

    Url Handle for Dealers Info

---
+++ src/com/neu/jan17/data/UrlHandle.java
@@ -0,0 +1,27 @@
+package com.neu.jan17.data;
+
+import java.awt.*;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+public class UrlHandle {
+    public void openWebpage(URI uri) {
+        Desktop desktop = Desktop.isDesktopSupported() ? Desktop.getDesktop() : null;
+        if (desktop != null && desktop.isSupported(Desktop.Action.BROWSE)) {
+            try {
+                desktop.browse(uri);
+            } catch (Exception e) {
+                System.out.println("Wrong URL");
+            }
+        }
+    }
+
+    public void openWebpage(URL url) {
+        try {
+            openWebpage(url.toURI());
+        } catch (URISyntaxException e) {
+            System.out.println("Wrong URL");
+        }
+    }
+}
